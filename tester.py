e1644a330288d46bce7ec4dcc969fdd240490fe9
Jessica Burns <jessica.l.burns@gmail.com>
2020-03-24 15:48:58 -0700

    Teaching Xio about dynamically typed variables in Python! (*/ω＼*)

---
+++ tester.py
@@ -0,0 +1,4 @@
+people = ["Cynthia", "Jessica", "Chris", "Xiomara", "Eachann", "Olaiya"]
+
+for person in people:
+    print(f"WOW!  This person is {person}!  AMAZING!")
---
+++ variables.py
@@ -0,0 +1,18 @@
+#No variable
+print(1+1)
+
+#Dynamically typed variables in Python
+#First, number is an 'int' or Integer
+number = 1+6
+print(number)
+print(type(number))
+
+#Second, number is a 'str' or String
+number = "hello"
+print(number)
+print(type(number))
+
+#Third, number is a 'float' or a Floating Point number
+number = 5.7
+print(number)
+print(type(number))
