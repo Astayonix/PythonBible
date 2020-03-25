edfa4d4db5e4aed2da4677d369468b8e1ac9e6ee
Jessica Burns <jessica.l.burns@gmail.com>
2020-03-25 10:51:13 -0700

    Completed variable lesson╰(*°▽°*)╯╰(*°▽°*)

--- variables.py
+++ variables.py
@@ -1,9 +1,9 @@
 #No variable
-print(1+1)
+print(1 + 1)
 
 #Dynamically typed variables in Python
 #First, number is an 'int' or Integer
-number = 1+6
+number = 1 + 6
 print(number)
 print(type(number))
 
@@ -16,3 +16,35 @@ print(type(number))
 number = 5.7
 print(number)
 print(type(number))
+
+# More practice with variables
+first_number = 1 + 1 + 6
+print(first_number)
+second_number = 105 + 10
+print(second_number)
+
+total_number = first_number + second_number
+print(total_number)
+
+# Variable Naming Standards
+# myVariableName = CamelCase (start with lowercase, every new word gets Capitalized, no spaces, no underscores or hyphens) - Most popular in Java or Javascript
+aNewNumber = 10
+
+# MyVariableName = ProperCase (starts with uppercase, every new word gets Capitalized, no spaces, no underscores or hyphens)
+ANewNumber = 30
+
+# my_variable_name = SnakeCase (starts with lowercase, every new word is lowercase, no spaces, there are underscores, no hyphens) - Most propular in Python
+a_new_number = 20
+
+# variable assignment without assignment
+fourth_number # produces an error
+print(fourth_number)
+
+# variable assignment with assignment
+fourth_number = 10 # creates a variable named fourth_number with a value of 10
+print(fourth_number)
+
+# In this challenge, I want you to create a variable called x
+# Set the value for x equal to any whole number you choose using the assignment operator
+x = 50
+print(x)
