f1a497be55932cd9c9d259314cf10a50a7240de1
Jessica Burns <jessica.l.burns@gmail.com>
2020-03-26 10:47:07 -0700

    Starting the numbers module with Xio.

---
+++ numbers.py
@@ -0,0 +1,31 @@
+# In this module, I am teaching Xio about numbers and mathematical operations in Python
+
+# Addition
+print(2 + 3)
+
+# Subtraction
+print(10 - 4)
+
+# Multiplication
+print(8 * 8)
+
+# Division - By default creates floats in Python 3.  In Python 2, you need to specify float()
+print(9 / 3)
+
+# Modulo - The remainder of a division operation
+print(7 % 2)
+
+# Powers - A number times itself (2 * 2 * 2 * 2 * 2)
+print(2 ** 5)
+
+# Order of Operations - PEMDAS (parentheses, exponents, multiplication, division, addition, subtraction)
+print((9 + 1 * (3 ** 3)) % 5)
+
+# There are 2 basic numerical types in Python
+
+# Integer (int) - Whole numbers; takes up 6bytes of space in memory
+print(3)
+
+#Floats (float) - Decimal point or floating point numbers; takes up 16bytes of space in memory
+print(5.9)
+
